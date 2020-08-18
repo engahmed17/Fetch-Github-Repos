519f4b0b548204ffce9b449a8359c110340d67e8
Ahmed Mahmoud <engahmed.am@yahoo.com>
2020-08-18 17:05:04 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+let theInput = document.querySelector(".get-repos input");
+
+let getButton = document.querySelector(".get-button");
+
+let reposData = document.querySelector(".show-data");
+
+getButton.onclick = function () {
+
+    getRepos();
+}
+
+function getRepos() {
+
+    if (theInput.value == "") {
+
+        reposData.innerHTML = "<span>Please Write Github Username</span>"
+
+    } else {
+
+        fetch(`https://api.github.com/users/${theInput.value}/repos`)
+
+            .then((res) => {
+
+                return res.json();
+
+            })
+            .then((repos) => {
+
+                reposData.innerHTML = '';
+
+                repos.forEach(repo => {
+
+                    let mainDiv = document.createElement("div");
+
+                    let repoName = document.createTextNode(repo.name);
+
+                    mainDiv.appendChild(repoName);
+
+                    let theUrl = document.createElement('a');
+
+                    let theUrlText = document.createTextNode("Visit");
+
+                    theUrl.appendChild(theUrlText);
+
+                    theUrl.href = `https://github.com/${theInput.value}/${repo.name}`;
+
+                    theUrl.setAttribute('target', '_blank');
+
+                    mainDiv.appendChild(theUrl);
+
+                    mainDiv.className = 'repo-box';
+
+                    reposData.appendChild(mainDiv);
+                });
+            });
+    }
+}
